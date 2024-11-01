ba20a6527c480b5f7944b526f5a62e8f15e8f8ac
csuriti <csuriti@slu.edu>
2024-11-01 22:36:49 +0000

    LAB2_SUBMISSION

--- app.js
+++ app.js
@@ -4,28 +4,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const deleteButton = document.querySelector("#btnDelete");
     const saveButton = document.querySelector("#btnSave");
     generateUserList(userData, stocksData);
-deleteButton.addEventListener('click', (event) => {
+    saveButton.addEventListener('click', (event) => {
+      // we don't want the form to submit (since we will lose form state)
+      event.preventDefault();
+      const id = document.querySelector('#userID').value;
+      for (let i=0; i<userData.length; i++) {
+          if (userData[i].id == id) {
+              userData[i].user.firstname = document.querySelector('#firstname').value;
+              userData[i].user.lastname = document.querySelector('#lastname').value;
+              userData[i].user.address = document.querySelector('#address').value;
+              userData[i].user.city = document.querySelector('#city').value;
+              userData[i].user.email = document.querySelector('#email').value;     
+              generateUserList(userData, stocksData);
+          }  } });
+    deleteButton.addEventListener('click', (event) => {
 event.preventDefault();
 const userId = document.querySelector('#userID').value;
 const userIndex = userData.findIndex(user => user.id == userId);
 userData.splice(userIndex, 1);
 generateUserList(userData, stocksData);
 });
-saveButton.addEventListener('click', (event) => {
-event.preventDefault();
-const id = document.querySelector('#userID').value;
-const userIndex = userData.findIndex((user) => user.id == id);
-const newUsers = [   ...userData.slice(0, userIndex),
-  {     ...userData[userIndex],
-    user: {
-      firstname: document.querySelector("#firstname").value,
-      lastname: document.querySelector("#lastname").value,
-      address: document.querySelector("#address").value,
-      city: document.querySelector("#city").value,
-      email: document.querySelector("#email").value,   },  },
-  ...userData.slice(userIndex + 1)];
-  generateUserList(newUsers, stocksData);
-});
+
     function generateUserList(users, stocks) {
 const userList = document.querySelector('.user-list');
 userList.innerHTML = '';
